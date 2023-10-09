b6d00be7b160d5dbfca300f8b8cc9f48ed007291
Sarth77 <sahil3112109695@gmail.com>
2023-10-09 19:51:09 +0530

    Update env.mjs

--- env.mjs
+++ env.mjs
@@ -37,7 +37,7 @@ export const env = createEnv({
         NEXT_PUBLIC_FIREBASE_DATABASE_URL: z.string().url(),
 
         NEXT_PUBLIC_NEXT_PRODUCT_API: z.string().url(),
-        NEXT_PUBLIC_APP_URL: z.string().url(),
+        NEXT_PUBLIC_APP_URL: z.string(),
         NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
     },
 
